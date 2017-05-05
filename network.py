10b7df7acb9debf4120cab85ac7265759e8abc4d
Dave Sescleifer <dave@sescleifer.com>
2017-05-05 13:23:11 -0500

    Finished project!

--- network.py
+++ network.py
@@ -1,3 +1,4 @@
+import subprocess
 import random
 import os
 import gc
@@ -12,29 +13,28 @@ from keras.layers.normalization import BatchNormalization
 keras.backend.set_image_data_format('channels_first')
 
 def generate_data():
-  print('start')
-  while True:
-    files = [f for f in os.listdir('data/processed') if f.endswith('.npz')]
-    random.shuffle(files)
-    for filename in files:
-      print('loading data')
-      data = np.load('data/processed/%s' % filename)
-      examples = np.unpackbits(data['examples'], axis=1)
-      moves = np_utils.to_categorical(data['moves'], 361)
-      yield(examples, moves)
+  i = 326
+  while i < 1500:
+    if subprocess.run('gsutil -q ls gs://policy-network/work.{}.npz'.format(i).split()).returncode != 0:
+      i += 1
+      continue
+    subprocess.call('gsutil cp gs://policy-network/work.{}.npz data/processed/.'.format(i).split())
+
+    print('Loading dataset {}'.format(i))
+    data = np.load('data/processed/work.{}.npz'.format(i))
+    examples = np.unpackbits(data['examples'], axis=1)
+    moves = np_utils.to_categorical(data['moves'], 361)
+    i += 1
+    yield(examples, moves)
     
 if os.path.isfile('weights.h5'):
   model = keras.models.load_model('weights.h5')
 else:
   model = Sequential()
   
-  F = 64
-  model.add(Conv2D(F, (5, 5), input_shape=(24, 19, 19), padding='same', activation='elu'))
-  model.add(Conv2D(F, (3, 3), padding='same', activation='elu'))
-  model.add(Conv2D(F, (3, 3), padding='same', activation='elu'))
-  model.add(Conv2D(F, (3, 3), padding='same', activation='elu'))
-  model.add(Conv2D(F, (3, 3), padding='same', activation='elu'))
-  model.add(Conv2D(F, (3, 3), padding='same', activation='elu'))
+  model.add(Conv2D(96, (5, 5), input_shape=(24, 19, 19), padding='same', activation='elu'))
+  for i in range(17):
+    model.add(Conv2D(96, (3, 3), padding='same', activation='elu'))
   model.add(Flatten())
   model.add(Dense(361, activation='softmax'))
   
@@ -42,20 +42,23 @@ model.compile(
   loss='categorical_crossentropy',
   optimizer='sgd',
   metrics=['accuracy', keras.metrics.top_k_categorical_accuracy],
-  callbacks=[keras.callbacks.TensorBoard()],
+  callbacks=[keras.callbacks.ModelCheckpoint('/home/dave/policy-network/checkpoints/weights.{epoch:02d}-{val_loss:.2f}.h5')],
 )
 
+print(model.summary())
 generator = generate_data()
 while True:
   gc.collect()
   examples, moves = next(generator)
-  model.fit(
+  history = model.fit(
     examples,
     moves,
     validation_split=0.1,
     epochs=1,
     shuffle=True,
   )
-
+  with open('log.txt', 'a') as log:
+    log.write(str(history.history) + "\n")
   model.save('weights.h5')
+
 
